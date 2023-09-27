3ca680d027e0f83245f11d395a1b1156d5d088c0
amarzouk2023 <138410585+amarzouk2023@users.noreply.github.com>
2023-09-27 18:40:09 +0100

    Update TestClass.java
    Second modif in main

--- TestClass.java
+++ TestClass.java
@@ -1,6 +1,6 @@
 public class TestClass {
 
 	public static void main(String[] args) {
-		System.out.println("From GitHub test webhook!!!");
+		System.out.println("From GitHub test webhook 27 Sept 2023!!!");
 	}
 }
